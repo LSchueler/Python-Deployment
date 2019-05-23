cd279bdcc72f7e85bf8527c0c5a820d05e8c4897
LSchueler <lennart.schueler@ufz.de>
2019-05-23 09:58:10 +0200

    Added a first test package

---
+++ deployment/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from deployment import deploy
+from deployment.deploy import add
+
+__all__ = ["add"]
---
+++ deployment/deploy.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+def add(a, b):
+    return a + b
+
+if __name__ == '__main__':
+    pass
